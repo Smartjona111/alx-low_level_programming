a01449ec0dd5915ec6586e6d306429d7c537fd38
Smartjona111 <113312601+Smartjona111@users.noreply.github.com>
2023-03-27 13:00:40 +0100

    Create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts_half - prints a string, followed by a new line,
+ * @str: pointer to the string to print
+ * Return: void
+*/
+
+void puts_half(char *str)
+{
+	int len, n, i;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
