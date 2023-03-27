0c9a7f76c45845ee9b0869bdc85c6d9c2708a9ef
Smartjona111 <113312601+Smartjona111@users.noreply.github.com>
2023-03-27 21:32:27 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -18,3 +18,14 @@ int _atoi(char *s)
 		if (s[i] == '-')
 			sig = sig * -1;
 		if (s[i] >= '0' && s[i] <= '9')
+                {
+			res = res * 10;
+			res -= (s[i] - '0');
+			brk = 1;
+		}
+		else if (brk == 1)
+			break;
+	}
+	res = sig * res;
+	return (res);
+}
